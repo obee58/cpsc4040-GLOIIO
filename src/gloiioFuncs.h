d39ca9aca10500c3dd992dec0de0b2c3417e7fb7
obee58 <obee58@gmail.com>
2022-11-01 16:23:20 -0400

    refactor attempt (needs testing)
    moved all non-gl functions of convolve.cpp to new .h and .cpp file to be used as "core" program

---
+++ src/gloiioFuncs.h
@@ -0,0 +1,68 @@
+#ifndef GLOIIO_OB_FUNCS_H
+#define GLOIIO_OB_FUNCS_H
+#include <OpenImageIO/imageio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+OIIO_NAMESPACE_USING;
+
+//assumed maximum value of pixel data - don't touch it kiddo
+#define MAX_VAL 255
+//preprocess macros aka math shorthand
+#define percentOf(a,max) ((double)(a)/(max))
+#define contigIndex(row,col,wid) (((row)*(wid))+(col)) //converts row & column into 1d array index
+#define maximum(x,y,z) ((x) > (y)? ((x) > (z)? (x) : (z)) : ((y) > (z)? (y) : (z)))
+#define minimum(x,y,z) ((x) < (y)? ((x) < (z)? (x) : (z)) : ((y) < (z)? (y) : (z)))
+
+//struct that holds RGBA 4-tuple of chars
+typedef struct pixel_rgba_t {
+	unsigned char red, green, blue, alpha;
+} pxRGBA;
+//struct that holds RGB 3-tuple of chars
+typedef struct pixel_rgb_t {
+	unsigned char red, green, blue;
+} pxRGB;
+//struct that holds HSV 3-tuple of doubles
+typedef struct pixel_hsv_t {
+	double hue, saturation, value;
+} pxHSV;
+//struct that holds RGBA 4-tuple of doubles (0~1)
+typedef struct floating_rgba_t {
+	double red, green, blue, alpha;
+} flRGBA;
+
+//struct to tie image spec and pixels together
+typedef struct image_rgba_uint8_t {
+	ImageSpec spec;
+	pxRGBA* pixels;
+} ImageRGBA;
+//struct representing .filt with calculated scale factor
+typedef struct convolve_filt_t {
+	int size; //NxN
+	double scale;
+	double* kernel;
+} RawFilter;
+
+void discardImage(ImageRGBA);
+void discardRawFilter(RawFilter);
+int clampInt(int,int,int);
+double clampDouble(double,double,double);
+pxRGB linkRGB(unsigned char,unsigned char,unsigned char);
+pxRGBA linkRGBA(unsigned char,unsigned char,unsigned char,unsigned char);
+pxHSV linkHSV(double,double,double);
+flRGBA percentify(pxRGBA);
+pxRGBA premult(pxRGBA);
+pxHSV RGBtoHSV(pxRGB);
+pxHSV RGBAtoHSV(pxRGBA);
+void readImage(string);
+void writeImage(string);
+void readFilter(string);
+int cloneImage(int);
+int removeImage(int);
+int removeFilt(int);
+void convolve(RawFilter);
+
+#endif
